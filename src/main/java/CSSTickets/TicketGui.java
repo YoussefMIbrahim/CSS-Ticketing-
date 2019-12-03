e0a215c4c850abbb81a549a3ff6dd55a5a816e0e
Youssef <kn1010az@go.minnstate.edu>
2019-12-03 17:53:35 -0600

    first commit

--- src/main/java/CSSTickets/TicketGui.java
+++ src/main/java/CSSTickets/TicketGui.java
@@ -1,4 +1,22 @@
 package CSSTickets;
 
+import javax.swing.*;
+
 public class TicketGui {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTextField clientNameTextField;
+    private JTextField starIdTextField;
+    private JTextField emailTextField;
+    private JTextField phoneNumberTextField;
+    private JTextField machineTextField;
+    private JTextArea descriptionTextArea;
+    private JTextField memberTextField;
+    private JButton submitButton;
+    private JComboBox searchByComboBox;
+    private JList ticketList;
+    private JComboBox orderByComboBox;
+    private JButton searchButton;
+    private JTextField searchByTextField;
+    private JButton loadAllTicketsButton;
 }
