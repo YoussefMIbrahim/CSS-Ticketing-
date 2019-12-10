2dbc8e139c8b23929b445f11ac3442fca7bc7393
Youssef <kn1010az@go.minnstate.edu>
2019-12-10 14:14:36 -0600

    new frame

--- src/main/java/CSSTickets/testFrame.java
+++ src/main/java/CSSTickets/testFrame.java
@@ -4,4 +4,14 @@ import javax.swing.*;
 
 public class testFrame {
     private JPanel mainPanel;
+    private JTextField clientNameTextField;
+    private JTextField starIdTextField;
+    private JTextField emailTextField;
+    private JTextField phoneNumberTextField;
+    private JTextField deviceModelTextField;
+    private JTextArea descriptionTextArea;
+    private JTextArea resolutionTextArea;
+    private JButton exitButton;
+    private JButton saveAndExitButton;
+    private JTextField clubMemberNameTextField;
 }
