b783cdb5fce2ab6467151d7d155ef895d2af5f0f
Youssef <kn1010az@go.minnstate.edu>
2019-12-11 13:28:04 -0600

    adding comments

--- src/main/java/CSSTickets/Controller.java
+++ src/main/java/CSSTickets/Controller.java
@@ -6,12 +6,14 @@ import java.util.Vector;
 
 public class Controller {
 
+    // getting access to the store class
     TicketStore store;
 
     Controller(TicketStore store){
         this.store = store;
     }
 
+    //passing all the tickets from store to gui
     protected List<Ticket> loadAllTicketsFromTicketStore(){
 
         List<Ticket> allTickets = store.getAllTickets();
@@ -19,7 +21,7 @@ public class Controller {
         return allTickets;
 
     }
-
+    // passing ticket info from gui to store
     protected boolean addTicket(Ticket newTicket){
 
         try{
@@ -32,20 +34,24 @@ public class Controller {
 
     }
 
+    // taking search terms from gui and using them to search in store then returning the matching tickets to gui
     protected List<Ticket> searchByCategory(String searchTerm, String searchCategory){
         List<Ticket> matchingTickets = store.searchByCategory(searchTerm,searchCategory);
         return matchingTickets;
     }
 
+    // passing the rowid for the ticket marked for deletion from gui to store
     protected void deleteTicket(int rowId){
         store.deleteTicket(rowId);
     }
 
+    // passing row id for ticket that we're trying to get from DB to store
     protected Ticket getTicketById(int rowId){
         Ticket ticket = store.getTicketById(rowId);
         return ticket;
     }
 
+    // giving store a new ticket from the gui to input into the database through store
     protected void updateTicket(Ticket ticket){
         store.updateTicket(ticket);
     }
--- src/main/java/CSSTickets/DBConfig.java
+++ src/main/java/CSSTickets/DBConfig.java
@@ -1,6 +1,6 @@
 package CSSTickets;
 
 public class DBConfig {
-
+    // path for the databse
     static String DBUri = "jdbc:sqlite:ticket.sqlite";
 }
